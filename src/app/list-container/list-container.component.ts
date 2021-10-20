ef1dac9e2554a0318b6efb28ceb32761da51505a
Ilenia96 <ilenianese96@gmail.com>
2021-10-20 23:25:35 +0200

    fix code

--- src/app/list-container/list-container.component.ts
+++ src/app/list-container/list-container.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-list-container',
   templateUrl: './list-container.component.html',
   styleUrls: ['./list-container.component.scss']
 })
 export class ListContainerComponent implements OnInit {
-items = ['Cosa vuoi annotare oggi?'];
+items = ['Un esempio, scrivere codice!']
+
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  addItem(newItem: string) {
-    this.items.push(newItem);
+  addItem(newItem : string) {
+  this.items.push(newItem);
   }
+
 
   clearList() {
     this.items = [];
-
-
   }
 
 
